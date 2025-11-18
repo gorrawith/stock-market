e535efcada04a0daab01dd3556dcc876b7ef814d
b2time <gorrawith17@gmail.com>
2025-11-18 15:26:44 +0700

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from '@/components/ui/button'
+
+const Home =() =>{
+  return(
+    <div className='flex min-h-screen home-wrapper'>
+      {/* <Button>Click me</Button> */}
+      Home
+    </div>
+  )
+}
+
+export default Home
---
+++ lib/constants.ts
@@ -0,0 +1,5 @@
+export const NAV_ITEMS =[
+    {href: '/',label: 'Dashboard'},
+    {href: '/search', label: 'search'},
+    {href: '/watchlist',label: 'Watchlist'},
+]
